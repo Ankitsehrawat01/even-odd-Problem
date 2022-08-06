bfc66a80f052e7a98b777250238f7795ad5b3035
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-06 17:52:15 +0530

    even_odd_problem

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Print the number is Even or Odd");
+Console.Write("Enter a Number : ");
+int number =(int)Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine("Number is Even");
+}
+else
+{
+    Console.WriteLine("Number is Odd");
+}
